1dc8a56ef641f7d5dd4d3d44e2ed1325619c377d
borro56 <nicolas.borromeo@gmail.com>
2020-02-01 01:55:28 -0300

    +Mouse attraction system

---
+++ Assets/Scripts/ECS/Systems/AttractorSystem.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AttractorSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var cam = Camera.main;
+        if (cam == null || !Mouse.current.leftButton.isPressed) return inputDeps; //TODO: Replace with new Unity Input
+        
+        var force = AttractionGlobal.Instance.Force;
+        var minDistance = AttractionGlobal.Instance.Distance;
+        
+        var deltaTime = Time.DeltaTime;
+        var distanceToFloor = cam.transform.position.y;
+        var mousePos = new float3(Mouse.current.position.ReadValue(), distanceToFloor);
+        float3 mousePos3D = cam.ScreenToWorldPoint(mousePos);
+        
+        Debug.DrawRay(mousePos3D, Vector3.forward);
+
+        return Entities.WithAll<AttractorTag>().ForEach((ref PhysicsVelocity vel, in Translation trans) =>
+        {
+            var diff = mousePos3D - trans.Value;
+            var lengthSq = math.lengthsq(diff);
+            if (lengthSq > minDistance * minDistance) return;
+
+            var dist = math.sqrt(lengthSq);
+            var forceCoef = 1 - dist / minDistance;
+            var dirToMouse = math.normalize(diff);
+            vel.Linear += dirToMouse * force * forceCoef * deltaTime;
+
+        }).Schedule(inputDeps);
+    }
+}
---
+++ Assets/Scripts/Globals/AttractionGlobal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractionGlobal : MonoBehaviour
+{
+    public static AttractionGlobal Instance;
+
+    public float Distance = 10;
+    public float Force = 100;
+
+    void Awake()  { Instance = this; }
+}

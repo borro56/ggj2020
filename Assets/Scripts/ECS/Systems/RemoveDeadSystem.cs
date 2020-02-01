9efdd0b2741cea18a3f9a3301bae9eec791d1360
borro56 <nicolas.borromeo@gmail.com>
2020-02-01 11:56:54 -0300

    +Added destroy on zero life system

---
+++ Assets/Scripts/ECS/Systems/RemoveDeadSystem.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+public class RemoveDeadSystem : JobComponentSystem
+{
+    EndSimulationEntityCommandBufferSystem bufferSystem;
+    
+    protected override void OnCreate()
+    {
+        bufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var buffer = bufferSystem.CreateCommandBuffer().ToConcurrent();
+
+        var jobHandle = Entities.ForEach((Entity entity, in Life life) =>
+        {
+            if(life.amount <= 0)
+                buffer.DestroyEntity(0, entity);
+        }).Schedule(inputDeps);
+        
+        bufferSystem.AddJobHandleForProducer(jobHandle);
+        return jobHandle;
+    }
+}

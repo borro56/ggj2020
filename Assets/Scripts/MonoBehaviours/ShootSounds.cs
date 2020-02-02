99a08bc6bfede1a96ba5b582bd4242cd4a4ceb9f
borro56 <nicolas.borromeo@gmail.com>
2020-02-02 14:27:31 -0300

    +Added shoot sounds

---
+++ Assets/Scripts/MonoBehaviours/ShootSounds.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class ShootSounds : MonoBehaviour
+{
+    [SerializeField] AudioClip shootSound;
+
+    float delay;
+    List<AudioSource> shootSounds = new List<AudioSource>();
+    EntityQuery query;
+
+    void Start()
+    {
+        query = World.DefaultGameObjectInjectionWorld.EntityManager.CreateEntityQuery(typeof(Ship));
+    }
+
+    void Update()
+    {
+        var amount = query.CalculateEntityCount();
+        
+        for (int i = amount; i < shootSounds.Count; i++)
+        {
+            Destroy(shootSounds[i].gameObject);
+        }
+
+        for (int i = amount; i < shootSounds.Count; i++)
+        {
+            shootSounds.RemoveAt(shootSounds.Count - 1);
+        }
+
+        delay -= Time.deltaTime;
+        if (delay < 0)
+        {
+            delay = shootSound.length / 2;
+
+            if (shootSounds.Count < amount)
+            {
+                var audio = new GameObject().AddComponent<AudioSource>();
+                audio.clip = shootSound;
+                audio.loop = true;
+                audio.volume = 0.25f;
+                audio.pitch = Random.Range(0.5f, 1.5f);
+                audio.Play();
+                shootSounds.Add(audio);
+            }
+        }
+    }
+}

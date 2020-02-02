dd918a6b270e7032a9341c70a628f08265dd675c
DonForse <maxi_rejlis@hotmail.com>
2020-02-02 14:46:41 -0300

    score y timer

---
+++ Assets/Scripts/MonoBehaviours/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public float score;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        score += Time.deltaTime* UnityEngine.Random.Range(1,130) * 10;
+        GameUI.Instance.UpdateScore((int)score);
+        GameUI.Instance.UpdateTimer(TimeSpan.FromSeconds(Time.time));
+    }
+}

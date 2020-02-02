ab870bc6b930dcae4a853c0d48f0ac3d22c029a2
borro56 <nicolas.borromeo@gmail.com>
2020-02-02 13:48:04 -0300

    +Added lose condition

---
+++ Assets/Scripts/MonoBehaviours/LoseCondition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TMPro;
+using Unity.Entities;
+using UnityEngine;
+
+public class LoseCondition : MonoBehaviour
+{
+    EntityQuery query;
+    float time = -1;
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] GameObject resetButton;
+
+    public bool Won => time >= 0;
+
+    void Awake()
+    {
+        query = World.DefaultGameObjectInjectionWorld.EntityManager.CreateEntityQuery(typeof(Ship));
+    }
+
+    void Update()
+    {
+        if (!Won && query.CalculateEntityCount() <= 0)
+        {
+            time = Time.realtimeSinceStartup;
+            text.text = "You survived " + time.ToString("000") + " seconds";
+            text.gameObject.SetActive(true);
+            //resetButton.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/MonoBehaviours/ResetScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour
+{
+    public void ResetLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+}

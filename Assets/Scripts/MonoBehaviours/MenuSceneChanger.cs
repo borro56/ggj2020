acaf46a6f9d6efe52a62a70bfe47c007f7c54845
patricio.guerra <patricio.guerra@etermax.com>
2020-02-02 11:17:21 -0300

    Added main menu

---
+++ Assets/Scripts/MonoBehaviours/MenuSceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MonoBehaviours
+{
+    public class MenuSceneChanger : MonoBehaviour
+    {
+        public float timeUntilSceneChange;
+        public string scene;
+
+        private bool finished = false;
+        private void Update()
+        {
+            if (Time.time > timeUntilSceneChange && !finished)
+            {
+                finished = true;
+                SceneManager.LoadScene(scene, LoadSceneMode.Single);
+            }
+        }
+    }
+}
